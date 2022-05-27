911aa4ad8559210d4bb108ddc129141b33319d8f
Emmanuel Olisah <netesy1@gmail.com>
2022-05-27 09:33:51 +0400

    Author section

--- stories/authors.py
+++ stories/authors.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.storyDashboard.as_view(), name='dashboard'),
     path('list/', views.storyList.as_view(), name='list'),
     path('create/', views.createStory.as_view(), name='create'),
+    path('story/<pk>/', views.viewStory.as_view(), name='show'),
     path('story/<pk>/update/', views.updateStory.as_view(), name='update'),
     path('story/<int:pk>/delete/', views.deleteStory.as_view(), name='delete'),
     path('story/<int:pk>/chapter/new/', views.createChapter.as_view(), name='createchapter'),
