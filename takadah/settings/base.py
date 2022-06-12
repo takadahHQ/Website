5b0aab81e8086b76c2a8c79c6901fecd1422daa6
Emmanuel Olisah <netesy1@gmail.com>
2022-06-12 08:57:59 +0400

    Static

--- takadah/settings/base.py
+++ takadah/settings/base.py
@@ -174,6 +174,8 @@ USE_TZ = True
 
 TAILWIND_APP_NAME = 'owliver'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Default primary key field type
