c4294cdf709f4d6efa8c7be8152e0c0a304c321a
Netesy [Maximus] Emmanuel <netesy@users.noreply.github.com>
2022-11-10 23:27:28 +0400

    Fixed core model

--- core/models.py
+++ core/models.py
@@ -41,12 +41,7 @@ class idModel(models.Model):
         abstract = True
 
 
-class Kycs(models.Model):
-    status_choices = (
-        ('active', 'Active'),
-        ('inactive', 'Inactive'), 
-        )
-    id = models.BigAutoField(primary_key=True)
+class Kycs(idModel, statusModel, timeStampModel):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=70)
     last_name = models.CharField(max_length=70)
@@ -58,8 +53,6 @@ class Kycs(models.Model):
     rejected_reason = RichTextField(blank=True, null=True)
     approved_at = models.DateTimeField(blank=True, null=True)
     rejected_at = models.DateTimeField(blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name_plural =  'kycs'
@@ -67,18 +60,9 @@ class Kycs(models.Model):
     def __str__(self):
         return self.first_name
 
-class KycDocuments(models.Model):
-    status_choices = (
-        ('active', 'Active'),
-        ('inactive', 'Inactive'), 
-        )
-    id = models.BigAutoField(primary_key=True)
+class KycDocuments(idModel, nameModel, statusModel, timeStampModel):
     kyc = models.ForeignKey(Kycs, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255, blank=True, null=True)
     path = models.CharField(max_length=255, blank=True, null=True)
-    status = models.CharField(max_length=100, choices=status_choices, default='active')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.kyc.first_name +' '+ self.name
@@ -87,18 +71,9 @@ class KycDocuments(models.Model):
         verbose_name_plural =  'kyc_documents'        
 
 
-class Languages(models.Model):
-    status_choices = (
-        ('active', 'Active'),
-        ('inactive', 'Inactive'), 
-        )
-    id = models.BigAutoField(primary_key=True)
+class Languages(idModel, nameModel, statusModel, timeStampModel):
     code = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
     native_name = models.CharField(max_length=255)
-    status = models.CharField(max_length=100, choices=status_choices, default='active')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -106,25 +81,17 @@ class Languages(models.Model):
         verbose_name_plural =  'languages'
 
 
-class Menus(models.Model):
-    status_choices = (
-        ('active', 'Active'),
-        ('inactive', 'Inactive'), 
-        )
+class Menus(idModel, nameModel, statusModel, timeStampModel):
     position_choices = (
         ('header', 'Header'),
         ('footer', 'Footer'), 
         )
-    id = models.BigAutoField(primary_key=True)
-    name = models.CharField(max_length=255)
+
     position = models.CharField(max_length=100, choices=position_choices, default='header')
     parent_id = models.ForeignKey('Menus', on_delete=models.CASCADE, blank=True, null=True)
     image = models.CharField(max_length=255, blank=True, null=True)
     icon = models.CharField(max_length=255, blank=True, null=True)
     link = models.CharField(max_length=255)
-    status = models.CharField(max_length=100, choices=status_choices, default='active')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -133,13 +100,7 @@ class Menus(models.Model):
         verbose_name_plural =  'menus'
     
 
-class Settings(models.Model):
-    status_choices = (
-        ('active', 'Active'),
-        ('inactive', 'Inactive'), 
-        )
-    id = models.BigAutoField(primary_key=True)
-    name = models.CharField(max_length=255)
+class Settings(idModel, nameModel, statusModel, timeStampModel):
     tagline = models.TextField(blank=True, null=True)
     logo = models.CharField(max_length=255, blank=True, null=True)
     phone = models.CharField(max_length=255)
@@ -147,9 +108,6 @@ class Settings(models.Model):
     color = models.CharField(max_length=255)
     theme = models.CharField(max_length=255)
     footer = models.CharField(max_length=255)
-    status = models.CharField(max_length=100, choices=status_choices, default='active')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -157,19 +115,11 @@ class Settings(models.Model):
     class Meta:
         verbose_name_plural =  'settings'
 
-class Socials(models.Model):
-    status_choices = (
-        ('active', 'Active'),
-        ('inactive', 'Inactive'), 
-        )
-    id = models.BigAutoField(primary_key=True)
+class Socials(idModel, statusModel, timeStampModel):
     name = models.CharField(max_length=255)
     icon = models.TextField(blank=True, null=True)
     image = models.CharField(max_length=255, blank=True, null=True)
     link = models.CharField(max_length=255)
-    status = models.CharField(max_length=100, choices=status_choices, default='active')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -178,8 +128,7 @@ class Socials(models.Model):
         verbose_name_plural =  'socials'
 
 
-class Transactions(models.Model):
-    id = models.BigAutoField(primary_key=True)
+class Transactions(idModel, timeStampModel):
     payable_type = models.CharField(max_length=255)
     payable_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     wallet = models.ForeignKey('Wallets', models.DO_NOTHING)
@@ -188,8 +137,6 @@ class Transactions(models.Model):
     confirmed = models.IntegerField()
     meta = models.JSONField(blank=True, null=True)
     uuid = models.CharField(unique=True, max_length=36)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.amount
@@ -198,12 +145,12 @@ class Transactions(models.Model):
         verbose_name_plural =  'transactions'
 
 
-class Transfers(models.Model):
+class Transfers(idModel, timeStampModel):
     status_choices = (
         ('active', 'Active'),
         ('inactive', 'Inactive'), 
         )
-    id = models.BigAutoField(primary_key=True)
+
     from_type = models.CharField(max_length=255)
     from_id = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='sender', on_delete=models.CASCADE)
     to_type = models.CharField(max_length=255)
@@ -215,8 +162,6 @@ class Transfers(models.Model):
     discount = models.DecimalField(max_digits=64, decimal_places=0)
     fee = models.DecimalField(max_digits=64, decimal_places=0)
     uuid = models.CharField(unique=True, max_length=36)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.fee
@@ -310,19 +255,15 @@ class Users(AbstractUser):
         verbose_name_plural =  'users'
 
 
-class Wallets(models.Model):
-    id = models.BigAutoField(primary_key=True)
+class Wallet(idModel, nameModel, timeStampModel):
     holder_type = models.CharField(max_length=255)
     holder_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255)
     uuid = models.CharField(unique=True, max_length=36)
     description = models.CharField(max_length=255, blank=True, null=True)
     meta = models.JSONField(blank=True, null=True)
     balance = models.DecimalField(max_digits=64, decimal_places=0)
     decimal_places = models.PositiveSmallIntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -332,15 +273,11 @@ class Wallets(models.Model):
         unique_together = (('holder_type', 'holder_id', 'slug'),)
 
 
-class Site(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    name = models.CharField(max_length=255)
+class Site(idModel, nameModel, timeStampModel):
     hero_header = models.CharField(max_length=255)
     hero_text = models.CharField(max_length=255)
     newsletter_header = models.CharField(max_length=255)
     newsletter_text = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -349,9 +286,8 @@ class Site(models.Model):
         verbose_name_plural =  'site'
        # unique_together = (('holder_type', 'holder_id', 'slug'),)
 
-class Bank(idModel, statusModel, timeStampModel):
+class Bank(idModel, nameModel, statusModel, timeStampModel):
     holder = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
     holder_name = models.CharField(max_length=255)
     swift = models.CharField(max_length=255)
     iban = models.CharField(max_length=255)
