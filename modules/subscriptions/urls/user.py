3a37e26aa5322ba4cac0b5457735d739b1702fa6
Netesy [Maximus] Emmanuel <netesy@users.noreply.github.com>
2023-02-20 23:10:41 +0400

    fixed the sponsorship

--- modules/subscriptions/urls/user.py
+++ modules/subscriptions/urls/user.py
@@ -2,15 +2,23 @@ from django.urls import path
 from modules.subscriptions.views import user as views
 
 urls = [
-    path("sponsors/", views.SponsorListView.as_view(), name="sponsor-list"),
-    path("sponsors/new/", views.SponsorCreateView.as_view(), name="sponsor-create"),
     path(
-        "sponsors/<int:pk>/edit/",
+        "<str:type>/<story>/sponsors/",
+        views.SponsorListView.as_view(),
+        name="sponsor-list",
+    ),
+    path(
+        "<str:type>/<story>/sponsors/new/",
+        views.SponsorCreateView.as_view(),
+        name="sponsor-create",
+    ),
+    path(
+        "<str:type>/<story>/sponsors/<int:pk>/edit/",
         views.SponsorUpdateView.as_view(),
         name="sponsor-update",
     ),
     path(
-        "sponsors/<int:pk>/delete/",
+        "<str:type>/<story>/sponsors/<int:pk>/delete/",
         views.SponsorDeleteView.as_view(),
         name="sponsor-delete",
     ),
