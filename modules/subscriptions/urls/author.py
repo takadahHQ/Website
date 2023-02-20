8fcd41b2a822e24a7f2fa6d4e05fa345fac40c0b
Netesy [Maximus] Emmanuel <netesy@users.noreply.github.com>
2023-02-20 23:11:29 +0400

    fixed for packages too

--- modules/subscriptions/urls/author.py
+++ modules/subscriptions/urls/author.py
@@ -4,17 +4,29 @@ from modules.subscriptions.views import author as views
 app_name = "author"
 urls = [
     # Package URLs
-    path("packages/", views.PackageListView.as_view(), name="package-list"),
-    path("packages/new/", views.PackageCreateView.as_view(), name="package-create"),
     path(
-        "packages/<int:pk>/edit/",
+        "<str:type>/<story>/packages/",
+        views.PackageListView.as_view(),
+        name="package-list",
+    ),
+    path(
+        "<str:type>/<story>/packages/new/",
+        views.PackageCreateView.as_view(),
+        name="package-create",
+    ),
+    path(
+        "<str:type>/<story>/packages/<int:pk>/edit/",
         views.PackageUpdateView.as_view(),
         name="package-update",
     ),
     path(
-        "packages/<int:pk>/delete/",
+        "<str:type>/<story>/packages/<int:pk>/delete/",
         views.PackageDeleteView.as_view(),
         name="package-delete",
     ),
-    path("sponsors/", views.SponsorListView.as_view(), name="sponsor-list"),
+    path(
+        "<str:type>/<story>/sponsors/",
+        views.SponsorListView.as_view(),
+        name="sponsor-list",
+    ),
 ]
