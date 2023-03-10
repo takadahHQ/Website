78e40163da6edf3ffd86bcae168888bbe99d19f3
Netesy [Maximus] Emmanuel <netesy@users.noreply.github.com>
2023-03-11 01:05:54 +0400

    removed the static and media urls

--- takadah/settings/development.py
+++ takadah/settings/development.py
@@ -45,7 +45,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # Local
-STATIC_URL = "static/"
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_URL = "media/"
+# STATIC_URL = "static/"
+# # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# MEDIA_URL = "media/"
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
