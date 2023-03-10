6360d2c53db70a0f5d1929081a0dcd3cdc78aba6
Netesy [Maximus] Emmanuel <netesy@users.noreply.github.com>
2023-03-11 02:19:37 +0400

    Reset the Database to verify source of the issue

--- takadah/settings/development.py
+++ takadah/settings/development.py
@@ -25,13 +25,17 @@ MIDDLEWARE += [
 # #Local
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "takadah",
-        "USER": "maximus",
-        "PASSWORD": "Z!nox2018",
-        "HOST": "takadah-test.czkumzmsnn55.ap-northeast-1.rds.amazonaws.com",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
+    # "default": {
+    #     "ENGINE": "django.db.backends.postgresql",
+    #     "NAME": "takadah",
+    #     "USER": "maximus",
+    #     "PASSWORD": "Z!nox2018",
+    #     "HOST": "takadah-test.czkumzmsnn55.ap-northeast-1.rds.amazonaws.com",
+    #     "PORT": "5432",
+    # }
 }
 
 # Password validation
