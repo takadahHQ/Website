e663f5dfe8f04b1702e368387e72930cdf805e06
Netesy [Maximus] Emmanuel <netesy@users.noreply.github.com>
2023-03-16 11:28:19 +0400

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+git pull origin master
+pip install -r requirements.txt
+python3 remote.py makemigrations
+python3 remote.py migrate
+python3 remote.py collectstatic
+sudo systemctl restart nginx
+sudo systemctl restart gunicorn
