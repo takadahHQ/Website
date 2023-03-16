ba2ecd2b121fcbe07b676df4c771e21bb06474b4
Netesy [Maximus] Emmanuel <netesy@users.noreply.github.com>
2023-03-16 11:08:39 +0400

    removed whitenoise

--- takadah/wsgi.py
+++ takadah/wsgi.py
@@ -6,7 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
-from whitenoise import WhiteNoise
+
 import os
 
 from django.core.wsgi import get_wsgi_application
