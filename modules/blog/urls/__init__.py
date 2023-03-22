8150376190fa783ee4d81735e8afe1311582c96c
Netesy [Maximus] Emmanuel <netesy@users.noreply.github.com>
2023-03-22 22:27:52 +0400

    Updated the footer with static info

--- modules/blog/urls/__init__.py
+++ modules/blog/urls/__init__.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.conf.urls import include
 from modules.blog.urls import staff, user
 
-app_name = "accounts"
+app_name = "blog"
 urlpatterns = [
     path("staff/blog", include(staff.urls)),
     path("blog", include(user.urls)),
--- modules/blog/urls/user.py
+++ modules/blog/urls/user.py
@@ -6,8 +6,8 @@ urls = [
     path("", user.PostListView.as_view(), name="list"),
     # path("/aaa", user.view_a, name="user.view_a"),
     # blog category
-    path("<slug:category>/", user.CategoryListView.as_view(), name="category"),
+    path("/<slug:category>/", user.CategoryListView.as_view(), name="category"),
     # blog tags
     # blog post
-    path("<slug:category>/<slug:slug>/", user.page, name="post"),
+    path("/<slug:category>/<slug:slug>/", user.page, name="post"),
 ]
