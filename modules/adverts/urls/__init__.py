07cee0113cb76cda12a37400e2fce83a02361bf5
Netesy [Maximus] Emmanuel <netesy@users.noreply.github.com>
2023-05-23 01:15:07 +0400

    updated

--- modules/adverts/urls/__init__.py
+++ modules/adverts/urls/__init__.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from django.conf.urls import include
-from modules.stories.urls import staff, user, author, personal
+from modules.adverts.urls import staff, user, author, personal
 
-app_name = "stories"
+app_name = "adverts"
 urlpatterns = [
-    path("staff/stories/", include((staff.urls, "staff"))),
+    path("staff/adverts/", include((staff.urls, "staff"))),
     path("read/", include(user.urls)),
     path("author/", include((author.urls, "author"))),
     path("user/", include(personal.urls)),
--- resources/static/js/site.js
+++ resources/static/js/site.js
@@ -28,3 +28,36 @@ themeToggleButton.addEventListener("click", () => {
     setLightTheme();
   }
 });
+
+
+let d = document.querySelectorAll(".switcher");
+localStorage.getItem("color-theme") === "dark" || !("color-theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches ? document.documentElement.classList.add("dark") : document.documentElement.classList.remove("dark");
+d.forEach(t => {
+    t.addEventListener("click", function() {
+        localStorage.getItem("color-theme") ? localStorage.getItem("color-theme") === "light" ? (document.documentElement.classList.add("dark"), localStorage.setItem("color-theme", "dark")) : (document.documentElement.classList.remove("dark"), localStorage.setItem("color-theme", "light")) : document.documentElement.classList.contains("dark") ? (document.documentElement.classList.remove("dark"), localStorage.setItem("color-theme", "light")) : (document.documentElement.classList.add("dark"), localStorage.setItem("color-theme", "dark"))
+    })
+});
+let i = document.querySelectorAll(".tab"),
+    a = document.querySelector(".tab-indicator"),
+    u = document.querySelectorAll(".panel"),
+    m = document.querySelectorAll(".panel-preview");
+a !== null && (a.style.width = i[0].getBoundingClientRect().width + "px", a.style.left = i[0].getBoundingClientRect().left - i[0].parentElement.getBoundingClientRect().left + "px");
+i.forEach(t => {
+    t.addEventListener("click", () => {
+        let r = t.getAttribute("aria-controls");
+        a.style.width = t.getBoundingClientRect().width + "px", a.style.left = t.getBoundingClientRect().left - t.parentElement.getBoundingClientRect().left + "px", u.forEach(o => {
+            let c = o.getAttribute("id");
+            r === c ? (o.classList.remove("invisible", "opacity-0", "scale-90"), o.classList.add("visible", "opacity-100", "scale-100")) : (o.classList.add("invisible", "opacity-0", "scale-90"), o.classList.remove("visible", "opacity-100", "scale-100")), m.forEach(e => {
+                let l = e.getAttribute("data-target");
+                r === l ? (e.classList.replace("translate-y-[100%]", "translate-y-0"), e.classList.replace("scale-75", "scale-100"), e.classList.replace("opacity-50", "opacity-100"), e.classList.replace("z-0", "z-10")) : (e.classList.replace("scale-100", "scale-75"), e.classList.replace("opacity-100", "opacity-50"), e.classList.replace("z-10", "z-0"), setTimeout(() => {
+                    e.classList.replace("translate-y-0", "translate-y-[100%]")
+                }, 300), clearTimeout())
+            })
+        })
+    })
+});
+const f = document.querySelector("#hamburger"),
+    g = document.querySelector("#navbar");
+f.addEventListener("click", () => {
+    g.classList.toggle("navbar-active")
+});
